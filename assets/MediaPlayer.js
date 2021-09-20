c8237763b21810937af6b2a9d8330669a3f14a4d
Luis Berenguer <luis01developer@gmail.com>
2021-09-20 20:08:47 +0200

    Añadimos plugins para mutear y autoplay

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,5 +1,14 @@
 function MediaPlayer(config) {
     this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
+  }
+
+  MediaPlayer.prototype._initPlugins = function() {
+      this.plugins.forEach(plugin => {
+          plugin.run(this);
+      });
   }
 
   MediaPlayer.prototype.play = function() {
@@ -18,4 +27,15 @@ function MediaPlayer(config) {
     }
   };
 
+  MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+  };
+
+  MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
+  };
+
   export default MediaPlayer;
+
+
+  
